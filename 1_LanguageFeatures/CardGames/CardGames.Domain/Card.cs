05f9ed917a120360de7eb5a498a897282f66de0c
MaysAbdulsamadPXL <Mays.Abdulsamad@student.pxl.be>
2023-12-11 21:40:06 +0100

    new file

---
+++ 1_LanguageFeatures/CardGames/CardGames.Domain/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace CardGames.Domain;
+
+public class Card : ICard
+{
+    public Card(CardSuit suit, CardRank rank)
+    {
+        Suit = suit;
+        Rank = rank;
+    }
+
+    public CardSuit Suit { get; }
+    public CardRank Rank { get; }
+}
